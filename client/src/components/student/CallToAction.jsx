9cd856c32331978e612c5aec8d1701a9ce5156c9
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-07-17 17:37:55 +0530

    Building calltoaction page

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {assets} from '../../assets/assets'
 function CallToAction() {
   return (
     <div>
-        <h1>Call to action</h1>
+      <h1>Knowledge at Your Fingertips.</h1>
+      <p>Build knowledge, sharpen skills, and stay ahead — anytime, anywhere, with TutorGrid's dynamic learning tools</p>
+      <div>
+        <button>Get stated</button>
+        <button>Learn More <img src={assets.arrow_icon} alt="" /></button>
+      </div>
     </div>
   )
 }
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CourseSection from '../../components/student/CourseSection'
 import TestimonialSection from '../../components/student/TestimonialSection'
+import CallToAction from '../../components/student/CallToAction'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Companies/>
         <CourseSection/>
         <TestimonialSection/>
+        <CallToAction/>
     </div>
   )
 }
