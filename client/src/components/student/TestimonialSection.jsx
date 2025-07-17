162b1a9f799f3f4d13cc5c3719c7ccec67aa7882
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-07-17 17:24:05 +0530

    completed testimonial section

--- client/src/components/student/TestimonialSection.jsx
+++ client/src/components/student/TestimonialSection.jsx
@@ -11,45 +11,44 @@ const TestimonialSection = () => {
         lives.
       </p>
 
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-14 w-fit">
-          {dummyTestimonial.map((testimonial, index) => (
-            <div
-              key={index}
-              className="text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-[0px_4px_15px_0px] overflow-hidden shadow-black/10"
-            >
-              <div className="flex items-center gap-4 px-5 py-4 bg-gray-500/10">
-                <img
-                  className="h-12 w-12 rounded-full"
-                  src={testimonial.image}
-                  alt={testimonial.name}
-                />
-                <div>
-                  <h1 className="text-lg font-medium text-gray-800">
-                    {testimonial.name}
-                  </h1>
-                  <p className="text-gray-800/80">{testimonial.role}</p>
-                </div>
-              </div>
-
-              <div className="p-5 pb-7">
-                <div className="flex gap-0.5">
-                  {[...Array(5)].map((_, i) => (
-                    <img
-                      key={i}
-                      className="w-4 h-4"
-                      src={
-                        i < testimonial.rating ? assets.star : assets.star_blank
-                      }
-                      alt="star"
-                    />
-                  ))}
-                </div>
-                <p className="text-gray-500 mt-5">{testimonial.feedback}</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-14 max-w-5xl">
+        {dummyTestimonial.map((testimonial, index) => (
+          <div
+            key={index}
+            className="text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-[0px_4px_15px_0px] overflow-hidden shadow-black/10"
+          >
+            <div className="flex items-center gap-4 px-5 py-4 bg-gray-500/10">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={testimonial.image}
+                alt={testimonial.name}
+              />
+              <div>
+                <h1 className="text-lg font-medium text-gray-800">
+                  {testimonial.name}
+                </h1>
+                <p className="text-gray-800/80">{testimonial.role}</p>
               </div>
             </div>
-          ))}
-        </div>
+
+            <div className="p-5 pb-7">
+              <div className="flex gap-0.5">
+                {[...Array(5)].map((_, i) => (
+                  <img
+                    key={i}
+                    className="w-4 h-4"
+                    src={
+                      i < testimonial.rating ? assets.star : assets.star_blank
+                    }
+                    alt="star"
+                  />
+                ))}
+              </div>
+              <p className="text-gray-500 mt-5">{testimonial.feedback}</p>
+            </div>
+            <a href="" className="text-blue-500 underline px-5">Read More</a>
+          </div>
+        ))}
       </div>
     </div>
   );
