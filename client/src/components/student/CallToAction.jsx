3d35162e14cf1a0e24c83cf413886469242af4fc
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-07-17 17:53:42 +0530

    completed call To Actions

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {assets} from '../../assets/assets'
 function CallToAction() {
   return (
-    <div>
-      <h1>Knowledge at Your Fingertips.</h1>
-      <p>Build knowledge, sharpen skills, and stay ahead — anytime, anywhere, with TutorGrid's dynamic learning tools</p>
-      <div>
-        <button>Get stated</button>
-        <button>Learn More <img src={assets.arrow_icon} alt="" /></button>
+    <div className='flex flex-col items-center gap-4 pt-10 pb-24 px-8 md: px-0'>
+      <h1 className='text-xl md:text-4xl text-gray-800 font-semibold'>Knowledge at Your Fingertips.</h1>
+      <p className='tex-gray-500 sm:text-sm'>Build knowledge, sharpen skills, and stay ahead — anytime, anywhere, with TutorGrid's dynamic learning tools</p>
+      <div className='flex items-center font-medium gap-6 mt-4'>
+        <button className='px-10 py-3 rounded-md text-white bg-blue-600'>Get stated</button>
+        <button className='flex items-center gap-2'>Learn More <img src={assets.arrow_icon} alt="" /></button>
       </div>
     </div>
   )
