cf90fbc8dd514a64b7fee936009f23fd2126aaee
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-07-20 00:11:00 +0530

    adding toggle in chapter content

--- client/src/pages/student/CourseDetail.jsx
+++ client/src/pages/student/CourseDetail.jsx
@@ -8,6 +8,7 @@ import humanizeDuration from 'humanize-duration';
 const CourseDetail = () => {
   const {id} = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [openSection, setOpenSection] = useState({});
   const {allcourses,averageRating ,calculateChapterTiming, calculateTotalCourseTiming, calculateTotalLectures} = useContext(AppContext);
   
   const fetchCourseDetails = async () => {
@@ -17,6 +18,13 @@ const CourseDetail = () => {
   useEffect(() => {
     fetchCourseDetails();
   }, []);
+
+  const toggleSection = (index) => {
+    setOpenSection(prev => ({
+      ...prev,
+      [index]: !prev[index]
+    }));
+  }
   
   return courseData ? (
     <>
@@ -44,22 +52,22 @@ const CourseDetail = () => {
             <div className='pt-5'>
               {courseData.courseContent.map((chapter, index) => (
                 <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none ' onClick={() => toggleSection(index)}>
                     <div className='flex items-center space-x-2'>
                       <img src={assets.down_arrow_icon} alt="" />
                       <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
                     <p className='text-sm md:text-default'>{chapter.chapterContent.length} Lectures - {calculateChapterTiming(chapter)}</p>
                   </div>
-                  <div className='overflow-hidden transition-all duration-300 max-h-96'>
+                  <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-screen' : 'max-h-0'}`}>
                     <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                       {chapter.chapterContent.map((lecture, lectureIndex) => (
                         <li key={lectureIndex} className='flex items-start gap-2 py-1 hover:bg-gray-100 cursor-pointer'>
                           <img src={assets.play_icon} alt="" className='w-4 h-4 mt-1'/>
-                          <div className='flex items-center justify-betweenw-full text-gray-800 text-xs md: text-default'>
+                          <div className='flex items-center justify-between w-full text-gray-800 text-xs md: text-default'>
                             <p>{lecture.lectureTitle}</p>
-                            <div>
-                              {lecture.isPreviewFree  && <p>Preview</p>}
+                            <div className='flex gap-2'>
+                              {lecture.isPreviewFree  && <p className='text-blue-500 cursor-pointer'>Preview</p>}
                               <p className='text-gray-500'>{humanizeDuration(lecture.lectureDuration * 60 * 1000,{units:['h','m']})}</p>
                             </div>
                           </div>
