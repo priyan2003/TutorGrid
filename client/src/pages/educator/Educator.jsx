431eb73bf0a25a860b742e9e1aa1cd4ad7e492de
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-07-24 14:11:59 +0530

    created side bar of educator page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
 
 function Educator() {
   return (
     <div className='text-defalut min-h-screen bg-white'>
       <Navbar/>
-      <div>
-        {<Outlet/>}
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex-1 p-4'>
+          {<Outlet/>}
+        </div>
       </div>
     </div>
   )
