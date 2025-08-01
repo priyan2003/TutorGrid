a3ac938bd6969f6e667a1b8ea873eb6928d4fee2
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-08-01 16:29:17 +0530

    replacing .env

--- Backend/controllers/webhooks.js
+++ Backend/controllers/webhooks.js
@@ -7,18 +7,17 @@ export const clerkWebhooks = async (req, res)=>{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
         await whook.verify(JSON.stringify(req.body), {
         "svix-id": req.headers["svix-id"],
-        "svix-timestamp": req. headers["svix-timestamp"],
-        "svix-signature": req. headers ["svix-signature"]
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"]
     })
     const {data, type} = req.body;
     switch (type) {
         case 'user.created': {
             const userData = {
-                _id:data.id,
+                _id: data.id,
                 email: data.email_addresses[0].email_address,
                 name: data.first_name + " " + data.last_name,
-                imageUrl: data.image_url,
-
+                imageUrl: data.image_url
             }
             await User.create(userData);
             res.json({});
@@ -32,13 +31,13 @@ export const clerkWebhooks = async (req, res)=>{
             }
             await User.findByIdAndUpdate(data.id,userData);
             res.json({});
-            res.json({});
+            break;
         }
         case 'user.deleted' : {
-        await User.findByIdAndDelete(data.id)
-        res. json({})
-        break;
-        }
+            await User.findByIdAndDelete(data.id)
+            res.json({})
+            break;
+            }
         default:
             break;
      }
