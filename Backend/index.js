1f586059973ecca85b35a87f1aeb40b9620d3323
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-08-01 15:17:26 +0530

    add vercel json

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // connect to db function
 
 app.get('/', (req, res)=>res.send("API Working"));
-
+app.post('/clerk',express.json(), clerkWebhooks)
 
 app.listen(PORT, ()=>{
     console.log(`Server running at Port ${PORT}`);
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -26,5 +26,4 @@ const UserSchema = new mongoose.Schema({
     ]
 
 },{timestamps:true});
-const User  = mongoose.model('User', UserSchema);
-export default User;
+export const User  = mongoose.model('User', UserSchema);
